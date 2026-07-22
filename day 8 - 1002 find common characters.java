1e037ec6f70b49d3092fab995c34d2d6b967e2cb
VIJAY-17-10 <vijaysuryaprakash85@gmail.com>
2026-07-22 22:03:37 +0530

    Add solution for finding common characters in words

---
+++ day 8 - 1002 find common characters.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<String> commonChars(String[] words) {
+        // Track the minimum frequency of each character across all words
+        int[] minFreq = new int[26];
+        Arrays.fill(minFreq, Integer.MAX_VALUE);
+
+        for (String word : words) {
+            int[] charFreq = new int[26];
+            for (char c : word.toCharArray()) {
+                charFreq[c - 'a']++;
+            }
+
+            // Keep the minimum count for each character
+            for (int i = 0; i < 26; i++) {
+                minFreq[i] = Math.min(minFreq[i], charFreq[i]);
+            }
+        }
+
+        // Build the final result list
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            while (minFreq[i] > 0) {
+                result.add(String.valueOf((char) (i + 'a')));
+                minFreq[i]--;
+            }
+        }
+
+        return result;
+    }
+}
